f199070b99ac897c8b2c8a1804791b896beac611
Brandon Murphy <eagleeye82689@gmail.com>
2013-04-04 16:29:20 -0500

    added list view

--- ParamedicClipboard/src/com/home/paramedicclipboard/HomeActivity.java
+++ ParamedicClipboard/src/com/home/paramedicclipboard/HomeActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 
 public class HomeActivity extends Activity {
 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
